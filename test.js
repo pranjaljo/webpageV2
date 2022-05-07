b51c08038ce073c9e5a9c8df223e0f54460e654e
pranjaljo <pro95.1298@gmail.com>
2022-05-07 11:17:26 +0530

    disable added

--- test.js
+++ test.js
@@ -1,26 +1,35 @@
 
+const input1 = document.getElementById('userName');
+const input2 = document.getElementById('userEmail')
+const input3 = document.getElementById('userMsg');
+input1.onchange = function () {
+    if (this.value != "" || this.value.length > 0) {
+        input2.classList.remove('disable');
+    } else {
+        input2.classList.add('disable');
+    }
+}
+input2.onchange = function () {
+    if (this.value != "" || this.value.length > 0) {
+        input3.classList.remove('disable');
+    } else {
+        input3.classList.add('disable');
+    }
+}
 
 sendEmail = function () {
-    const name = document.getElementById('userName').value
-    const email = document.getElementById('userEmail').value;
-    const msg = document.getElementById('userMsg').value;
+    const name = input1.value
+    const email = input2.value;
+    const msg = input3.value;
     const params = {
         from_name: name,
         email_id: email,
         message: msg,
     }
-    // emailjs.sendForm('service_gnaxs4m', 'template_sawg1sh', params)
-    //     .then(function () {
-    //         console.log('SUCCESS!');
-    //     }, function (error) {
-    //         console.log('FAILED...', error);
-    //     });
-    // console.log('sdsd');
-     
-    emailjs.send("service_gnaxs4m","template_sawg1sh",params)
-        .then(function(response) {
-           console.log('SUCCESS!', response.status, response.text);
-        }, function(error) {
-           console.log('FAILED...', error);
+    emailjs.send("service_gnaxs4m", "template_sawg1sh", params, "arl--CRC4wsn1bJXB")
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function (error) {
+            console.log('FAILED...', error);
         });
 }
